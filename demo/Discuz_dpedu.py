a80c371c0302fe958a5922bb1e4be4269935bd12
fraulen.shen@gmail.com <fraulen.shen@gmail.com>
2021-05-20 22:18:26 +0800

    写了一个压大鹏展翅后台登录接口的压测

---
+++ demo/Discuz_dpedu.py
@@ -0,0 +1,28 @@
+# 大鹏教育login页面登录性能测试
+from locust import HttpUser, TaskSet, task
+
+class Discuz_Login(TaskSet):
+
+    @task(1)
+    def index(self):
+        url = "/login"
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
+        self.client.get(url=url, headers=headers, name="登录首页")
+
+    @task(1)
+    def login(self):
+        url = "/system/personInfo"
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
+        data = {"grant_type": "password", "username": "18800000000", "password": "asd123456", "code": 0000}
+        with self.client.post(url, headers=headers, data=data, name="登录") as response:
+            print(response.text)
+            return response
+
+class Discuz_User(HttpUser):
+    tasks = [Discuz_Login]
+    # 等待时间
+    min_wait = 1000
+    max_wait = 3000
+    host = "https://test-zc.dapengjiaoyu.cn/#"
