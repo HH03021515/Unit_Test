8e4bde4bb26614304e5da2ea2b35a2299929af8e
Administrator <fraulen.shen@gmail.com>
2021-06-24 14:09:43 +0800

    还是locust

--- demo/locustDemo.py
+++ demo/locustDemo.py
@@ -1,38 +1,64 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpLocust, HttpUser, TaskSet, task, events
+import time, sys
 
-class UserBeHavior(TaskSet):
+class UserBehavior(HttpUser):
     """Locust任务集，定义每个locust行为"""
-    @task(1)    # 任务权重值，多任务可定义不同权重值
-    def get_root(self):
-        """模拟发送数据"""
-        response = self.client.get('/Hellooooo!', name='get_root')
-        if not response.ok:
-            print(response.text)
-            response.failure('Got wrong response')
+
+    def on_start(self):
+        print("运行压测前置条件")
+
+
+    def get_response(self, response):
+        """
+        获取返回
+        :param response:请求返回对象
+        :return:
+        """
+        start_time = int(time.time())
+        if response.status_code == 200:
+            events.request_success.fire(
+                request_type="recv",
+                name=sys._getframe().f_code.co_name,
+                response_time=int(time.time() - start_time) * 1000,
+                response_length=0
+            )
+        else:
+            events.request_failure.fire(
+                request_type="recv",
+                name=sys._getframe().f_code.co_name,
+                response_time=int(time.time() - start_time) * 1000,
+                response_length=0,
+                exception=f"Response Code Error! Code:{response.content}"
+            )
 
     @task(1)
-    def index1(self):
-        r = self.client.get('/test/index.html')
-        print(r.text)
-
-    @task(2)
-    def search1(self):
-        r = self.client.get('/test/search.html')
-        print(r.text)
+    def test_get(self):
+        self.client.get("https://baidu.com", name="打开百度首页")
 
 
+    @task(1)
+    def test_post(self):
+        """由于没有免费的post接口，暂时用百度搜索"""
+        self.client.post("https://baidu.com?wd=testerhome", name="使用百度搜索")
+
+class WebUser(TaskSet):
+    """性能测试配置，换算配置"""
+    task_set = UserBehavior  # Testcase类
+    host = "https://baidu.com"
+    min_wait = 1000
+    max_wait = 3000
 
 
-
-class TestLocust(HttpLocust):
-    """自定义locust类，可设置Locust的参数"""
-    tast_set = UserBeHavior
-    host = "https://www.baidu.com"  # 被测域名/服务器地址
-    min_wait = 5000
-    # 最小等待时间，至少等待多少秒后Locust选择执行一个任务。
-
-    max_wait = 9000
-    # 最大等待时间，最多等待多少秒后Locust选择执行一个任务。
+#
+# class TestLocust(HttpLocust):
+#     """自定义locust类，可设置Locust的参数"""
+#     tast_set = UserBeHavior
+#     host = "https://www.baidu.com"  # 被测域名/服务器地址
+#     min_wait = 5000
+#     # 最小等待时间，至少等待多少秒后Locust选择执行一个任务。
+#
+#     max_wait = 9000
+#     # 最大等待时间，最多等待多少秒后Locust选择执行一个任务。
 
 
 
