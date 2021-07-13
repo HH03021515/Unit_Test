805993895e1e2355159617c8f84184573000411c
Administrator <fraulen.shen@gmail.com>
2021-07-13 16:36:03 +0800

    https://blog.csdn.net/lizzy05/article/details/70792702

--- demo/DebugTalk.py
+++ demo/DebugTalk.py
@@ -1,14 +1,14 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpUser, TaskSet, task
 
 class WebsiteTask(TaskSet):
 
     def on_start(self):
-        self.client.post("/login",{
+        self.client.post("/login", {
             "username": "test",
             "password": "123456"
         })
 
-
+    # 请求两次/ 请求一次/about/
     @task(2)
     def index(self):
         self.client.get("/")
@@ -17,11 +17,22 @@ class WebsiteTask(TaskSet):
     def about(self):
         self.client.get("/about/")
 
+    @task(1)
+    def test_job1(self):
+        self.client.get("/job1")
 
-class WebsiteUser(HttpLocust):
+    @task(2)
+    def test_job2(self):
+        self.client.get("/job2")
 
-    task_set = WebsiteTask
+
+
+
+class WebsiteUser(HttpUser):
+
+    tasks = [WebsiteTask]
 
     host = "http://debugtalk.com"
+    # 两次请求间隔1-5秒的随机值
     min_wait = 1000
     max_wait = 5000
