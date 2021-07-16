59e1857923129e3ac171752b1dee582a97ee1007
Administrator <fraulen.shen@gmail.com>
2021-07-16 11:50:44 +0800

    添加了关联

--- demo/DebugTalk.py
+++ demo/DebugTalk.py
@@ -1,18 +1,50 @@
 # 运行测试时输入不同参数代表
+# -H, --host：被测系统的host，若在Terminal中不进行指定，就需要在Locust子类中通过host参数进行指定；
+# -f, --locustfile：指定执行的Locust脚本文件；
+# -P, --port：指定web端口，默认为8089.
 # 如果采用no_web形式，则需使用——no-web参数，并会用到如下几个参数:
 # -c, --clients：指定并发用户数；
 # -n, --num-request：指定总执行测试；
 # -r, --hatch-rate：指定并发加压速率，默认值位1。
 
+# 多进程分布式运行方式
+# 不管是单机多进程，还是多机负载模式，运行方式都是一样的，都是先运行一个master，再启动多个slave。
+# 启动master时，需要使用--master参数；同样的，如果要使用8089以外的端口，还需要使用-P, --port参数。
+# 如果slave与master不在同一台机器上，还需要通过--master-host参数再指定master的IP地址
+# $ locust -H http://debugtalk.com
+#       -f demo.py --slave --master-host=<locust_machine_ip>
+from lxml import etree
 from locust import HttpUser, TaskSet, task
 
 class WebsiteTask(TaskSet):
+    """
+    在某些请求中，需要携带之前从Server端返回的参数，
+    因此在构造请求时需要先从之前的Response中提取所需的参数。
+    """
+    @staticmethod
+    def get_session(html):
+        tree = etree.HTML(html)
+        return tree.xpath("//div[@class='btnbox']/input[@name='session']/@value")[0]
 
-    def on_start(self):
-        self.client.post("/login", {
-            "username": "test",
-            "password": "123456"
-        })
+    @task(10)
+    def test_login(self):
+        html = self.client.get('/login').text
+        username = 'user@compay.com'
+        password = '123456'
+        session = self.get_session(html)
+        payload = {
+            'username': username,
+            'password': password,
+            'session': session
+        }
+        self.client.post('/login', data=payload)
+
+
+    # def on_start(self):
+    #     self.client.post("/login", {
+    #         "username": "test",
+    #         "password": "123456"
+    #     })
 
     # 请求两次/ 请求一次/about/
     @task(2)
