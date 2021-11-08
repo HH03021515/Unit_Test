6de64f495f88630aa525446fef916a9e245f85b6
admin <fraulen.shen@gmail.com>
2021-11-08 17:33:49 +0800

    修改请求入参，以及接口URL

--- demo/Locust_demo/wechatPay_locust.py
+++ demo/Locust_demo/wechatPay_locust.py
@@ -36,12 +36,12 @@ class UserBehavior(TaskSet):
 
     @task(1)
     def test_getqueryPayOrder(self):
-        self.client.get("/orderquery/%7Bversion%7D/queryPayOrder?innerorderid=4A7ED092-E22C-4B2D-9B1A-E2B7290D40FA",
+        self.client.get("/orderquery/version/queryPayOrder?innerorderid=4A7ED092-E22C-4B2D-9B1A-E2B7290D40FA",
                         name='第三方订单查询接口（有结果）')
 
     @task(1)
     def test_getquertPayOrder_nodata(self):
-        self.client.get("/orderquery/%7Bversion%7D/queryPayOrder?innerorderid=4A7ED092-E22C-4B2D-9B1A-E2B7290D1111",
+        self.client.get("/orderquery/version/queryPayOrder?innerorderid=4A7ED092-E22C-4B2D-9B1A-E2B7290D1111",
                         name='第三方订单查询接口（无结果）')
 
     @task(1)
@@ -50,14 +50,11 @@ class UserBehavior(TaskSet):
             "Content-Type": "application/json",
         }
         payload = {
-            "extParam": {},
-            "message": "成功",
-            "orderId": "cc6acf1f-a25d-4d30-bf1f-b25c66bd3896",
-            "payNo": "0232da201eda48e5af85bf222e424e1f",
-            "payRecId": "p1636356830624018001001347015759",
-            "payStatus": 2,
-            "result": "true"
-        }
+        "appId": "wxcc603d9f0d54eaf0", "goodsTag": "", "innerOrderId": "C53129C6-634F-4F27-A0EA-2E9E60341CAF",
+         "openId": "", "payAmount": "11.00", "payBusinessType": "PARKING_TEMP",
+         "senceInfo": "{\"start_time\":\"2021-11-08T11:02:41+08:00\",\"plate_color\":\"BLUE\",\"device_id\":\"724\",\"end_time\":\"2021-11-08T14:05:53+08:00\",\"parking_id\":\"01000076482316363405638875052\",\"charging_duration\":10992,\"plate_number\":\"豫Q59M86\",\"parking_name\":\"李朗国际珠宝产业园\"}",
+         "tradeBizInfo": {"parkingCmbClearing": False, "parkingDirectClearing": False, "parkingId": 724,
+                          "parkingIsWanDa": False}}
         self.client.post("/paymentRest/crePayOrd4WxWithHoldRecord", json=payload, header=header, name='微信支付分代扣接口')
 
 class WebUser(FastHttpUser):
