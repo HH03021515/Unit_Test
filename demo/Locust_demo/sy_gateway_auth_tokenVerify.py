e47d5c06fc239fb404e273f836e894f76063ba8a
admin <fraulen.shen@gmail.com>
2021-11-17 19:46:05 +0800

    鉴权接口压测。返回请求的部分需要再修改。

---
+++ demo/Locust_demo/sy_gateway_auth_tokenVerify.py
@@ -0,0 +1,78 @@
+# sy网关鉴权接口压测
+
+import sys
+import time
+
+from locust import task, events, TaskSet
+from locust.contrib.fasthttp import FastHttpUser
+
+
+class auth_tokenVerify(TaskSet):
+
+    def on_start(self):
+        print('开始压测p平台网关。。。')
+
+    @task
+    def get_response(self, response):
+        """
+        获取返回
+        :param response:请求返回对象
+        :return:
+        """
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6InAwMDIiLCJmdWxsTmFtZSI6IuadjuaXrSIsImV4cCI6MTYzNzEzNDU4MSwidXVpZCI6ImUzYzk0ZTBjLTVlYWUtNDZkNS05YTc4LThiMmE0MTQzNGE4OCIsInVzZXJJZCI6IjQyMjUiLCJpYXQiOjE2MzcxMzA5ODF9.gRSXs6-ThH7eG1pyGKNdMYzg195UG8dIxpZr4TMg8eA'
+        }
+        body = {
+        }
+        start_time = int(time.time())
+        if response.status_code == 200:
+            events.request_success.fire(
+                request_type="recv",
+                name=sys._getframe().f_code.co_name,
+                response_time=int(time.time() - start_time) * 1000,
+                response_length=0
+            )
+        else:
+            events.request_failure.fire(
+                request_type="recv",
+                name=sys._getframe().f_code.co_name,
+                response_time=int(time.time() - start_time) * 1000,
+                response_length=0,
+                exception=f"Response Code Error! Code:{response.content}"
+            )
+        response = self.client.post("/authority/auth/1/tokenVerify", headers=headers, json=body, name='获取响应').text
+        assert '成功' in response
+
+    @task(1)
+    def test_auth_tokenVerify(self):
+        '''鉴权接口'''
+        header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6InAwMDIiLCJmdWxsTmFtZSI6IuadjuaXrSIsImV4cCI6MTYzNzEzNDU4MSwidXVpZCI6ImUzYzk0ZTBjLTVlYWUtNDZkNS05YTc4LThiMmE0MTQzNGE4OCIsInVzZXJJZCI6IjQyMjUiLCJpYXQiOjE2MzcxMzA5ODF9.gRSXs6-ThH7eG1pyGKNdMYzg195UG8dIxpZr4TMg8eA'
+        }
+        self.client.get(
+            "/authority/auth/1/tokenVerify", header=header, name='网关鉴权tokenVerify接口').text
+
+
+    # @task(1)
+    # def test_auth_tokenVerify_error(self):
+    #     '''鉴权接口传无效token'''
+    #     header = {
+    #         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+    #         'Accept-Encoding': 'gzip, deflate, br',
+    #         'token': '111111'
+    #     }
+    #     self.client.post(
+    #         "/authority/auth/1/tokenVerify", header=header, name='网关鉴权tokenVerify接口（无效token）').text
+
+
+
+class Web_gwp_tokenVerify(FastHttpUser):
+    tasks = [auth_tokenVerify]
+    min_wait = 1000
+    max_wait = 3000
+    host = "http://authority.intra.sit.etcp.net/"  # 网关host
