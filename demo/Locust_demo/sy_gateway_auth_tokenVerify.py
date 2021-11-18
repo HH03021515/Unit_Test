85dd32381f324c4a04290c016b71a086a7488250
admin <fraulen.shen@gmail.com>
2021-11-18 16:48:45 +0800

    鉴权接口压测。请求头入参从header改为headers。少写了个字母，发起请求失败，低级错误。。丢人

--- demo/Locust_demo/sy_gateway_auth_tokenVerify.py
+++ demo/Locust_demo/sy_gateway_auth_tokenVerify.py
@@ -12,8 +12,8 @@ class auth_tokenVerify(TaskSet):
     def on_start(self):
         print('开始压测p平台网关。。。')
 
-    def on_stop(self):
-        print('运行时间：', time.strftime("%Y-%m-%d %H:%M:%S"))
+    # def on_stop(self):
+    #     print('运行时间：', time.strftime("%Y-%m-%d %H:%M:%S"))
 
     def get_response(self, response):
         """
@@ -42,12 +42,19 @@ class auth_tokenVerify(TaskSet):
     def test_auth_tokenVerify(self):
         '''鉴权接口'''
         header = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-            'Accept-Encoding': 'gzip, deflate, br',
+            'Host': 'authority.intra.sit.etcp.net',
+            'Connection': 'keep-alive',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language:': 'zh-CN,zh;q=0.9',
             'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6InAwMDIiLCJmdWxsTmFtZSI6IuadjuaXrSIsImV4cCI6MTYzNzEzNDU4MSwidXVpZCI6ImUzYzk0ZTBjLTVlYWUtNDZkNS05YTc4LThiMmE0MTQzNGE4OCIsInVzZXJJZCI6IjQyMjUiLCJpYXQiOjE2MzcxMzA5ODF9.gRSXs6-ThH7eG1pyGKNdMYzg195UG8dIxpZr4TMg8eA'
         }
-        self.client.post(
-            "/authority/auth/1/tokenVerify", header=header, name='网关鉴权tokenVerify接口')
+        # response = self.client.post(
+        #     "auth/auth/1/tokenVerify", header=header, name='网关鉴权tokenVerify接口')
+        response = self.client.post("authority/auth/1/tokenVerify", headers=header, name='网关鉴权tokenVerify接口1')
+        print("Response status code: ", response.status_code)
+        print("Response content: ", response.text)
 
     # @task(1)
     # def test_auth_tokenVerify_error(self):
@@ -65,4 +72,5 @@ class Web_gwp_tokenVerify(FastHttpUser):
     tasks = [auth_tokenVerify]
     min_wait = 1000
     max_wait = 3000
-    host = "http://authority.intra.sit.etcp.net/"  # 网关host
+    # host = "http://gw-p.intra.sit.etcp.net/"  # 网关host
+    host = "http://authority.intra.sit.etcp.net/"
