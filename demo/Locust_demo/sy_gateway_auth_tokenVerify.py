f036abd50abc52e0abe6ae93bf4c08dbb8c0c90d
admin <fraulen.shen@gmail.com>
2021-11-18 17:27:40 +0800

    鉴权接口压测脚本，加了输出response的部分

--- demo/Locust_demo/sy_gateway_auth_tokenVerify.py
+++ demo/Locust_demo/sy_gateway_auth_tokenVerify.py
@@ -42,35 +42,28 @@ class auth_tokenVerify(TaskSet):
     def test_auth_tokenVerify(self):
         '''鉴权接口'''
         header = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-            'Host': 'authority.intra.sit.etcp.net',
-            'Connection': 'keep-alive',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language:': 'zh-CN,zh;q=0.9',
             'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6InAwMDIiLCJmdWxsTmFtZSI6IuadjuaXrSIsImV4cCI6MTYzNzEzNDU4MSwidXVpZCI6ImUzYzk0ZTBjLTVlYWUtNDZkNS05YTc4LThiMmE0MTQzNGE4OCIsInVzZXJJZCI6IjQyMjUiLCJpYXQiOjE2MzcxMzA5ODF9.gRSXs6-ThH7eG1pyGKNdMYzg195UG8dIxpZr4TMg8eA'
         }
-        # response = self.client.post(
-        #     "auth/auth/1/tokenVerify", header=header, name='网关鉴权tokenVerify接口')
-        response = self.client.post("authority/auth/1/tokenVerify", headers=header, name='网关鉴权tokenVerify接口1')
+        response = self.client.post(
+            "authority/auth/1/tokenVerify", headers=header, name='网关鉴权tokenVerify接口')
         print("Response status code: ", response.status_code)
         print("Response content: ", response.text)
 
-    # @task(1)
-    # def test_auth_tokenVerify_error(self):
-    #     '''鉴权接口传无效token'''
-    #     header = {
-    #         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-    #         'Accept-Encoding': 'gzip, deflate, br',
-    #         'token': '111111'
-    #     }
-    #     self.client.post(
-    #         "/authority/auth/1/tokenVerify", header=header, name='网关鉴权tokenVerify接口（无效token）').text
+    @task(1)
+    def test_auth_tokenVerify_error(self):
+        '''鉴权接口传无效token'''
+        header = {
+            'token': '111111'
+        }
+        response_error = self.client.post(
+            "authority/auth/1/tokenVerify", headers=header, name='网关鉴权tokenVerify接口（无效token）')
+        print("Response status code: ", response_error.status_code)
+        print("Response content: ", response_error.text)
 
 
 class Web_gwp_tokenVerify(FastHttpUser):
     tasks = [auth_tokenVerify]
     min_wait = 1000
     max_wait = 3000
-    # host = "http://gw-p.intra.sit.etcp.net/"  # 网关host
-    host = "http://authority.intra.sit.etcp.net/"
+    # host = "http://gw-p.intra.sit.etcp.net/"  # 网关host不带业务
+    host = "http://authority.intra.sit.etcp.net/"  # 带业务的
